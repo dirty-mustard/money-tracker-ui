1be1f4c0d20466aa4d94cba5dec4decb642948bf
angular-cli <angular-cli@angular.io>
2016-08-25 18:28:16 +0200

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MoneyTrackerUiPage } from './app.po';
+
+describe('money-tracker-ui App', function() {
+  let page: MoneyTrackerUiPage;
+
+  beforeEach(() => {
+    page = new MoneyTrackerUiPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
